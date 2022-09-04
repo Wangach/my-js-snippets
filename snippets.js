c12955f81128220ef4c399128d9e11b9d6fdb47c
John Kimani <jwangach@gmail.com>
2022-09-04 12:27:53 +0300

    Initial Commit

---
+++ snippets.js
@@ -0,0 +1,33 @@
+//get the current time
+function padTo2Digits(num) {
+  return num.toString().padStart(2, '0');
+}
+function convertMsToTime(milliseconds) {
+  let seconds = Math.floor(milliseconds / 1000);
+  let minutes = Math.floor(seconds / 60);
+  let hours = Math.floor(minutes / 60);
+
+  seconds = seconds % 60;
+  minutes = minutes % 60;
+
+  //  If you don't want to roll hours over, e.g. 24 to 00
+  //  comment (or remove) the line below
+  // commenting next line gets you `24:00:00` instead of `00:00:00`
+  // or `36:15:31` instead of `12:15:31`, etc.
+  hours = hours % 24;
+
+  /* return `${padTo2Digits(hours)}:${padTo2Digits(minutes)}:${padTo2Digits(
+    seconds,
+  )}`; */
+  console.log(
+  `${padTo2Digits(hours)}:${padTo2Digits(minutes)}:${padTo2Digits(
+    seconds,
+  )}`
+  );
+}
+const btn = document.getElementById('tme');
+btn.addEventListener('click', () => {
+let dte = new Date();
+let now = dte.getTime();
+convertMsToTime(now);
+})
